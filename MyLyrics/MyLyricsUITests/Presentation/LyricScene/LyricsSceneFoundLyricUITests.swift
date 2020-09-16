d08caf34e41bf71f13b47e13e5640ec03310f54b
Jorge Luis Peña Lopez <jlp7490@gmail.com>
2020-09-16 00:16:11 -0500

    Implementation is added of UI Test for form search lyric with results of lyric found and not found, also for get last lyric found

---
+++ MyLyrics/MyLyricsUITests/Presentation/LyricScene/LyricsSceneFoundLyricUITests.swift
@@ -0,0 +1,51 @@
+//
+//  LyricsSceneFoundLyricUITests.swift
+//  MyLyricsUITests
+//
+//  Created by Jorge Luis Peña López on 15/09/20.
+//  Copyright © 2020 Jorge Dev. All rights reserved.
+//
+
+import XCTest
+
+class LyricsSceneFoundLyricUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSearchingSongLyric() throws {
+        
+        let app = XCUIApplication()
+        let txtArtistIdentify = "txtArtist"
+        let txtSongTitleIdentify = "txtSongTitle"
+        let btnSearchLyricIdentify = "btnSearchLyric"
+        let viewLyricDetailIdentify = "viewLyricDetail"
+        
+        let artist = "Queen"
+        let songTitle = "Bohemian Rhapsody"
+        
+        app.textFields[txtArtistIdentify].tap()
+        if !app.keys["a"].waitForExistence(timeout: 5) {
+            XCTFail("The keyboard could not be found. Use keyboard shortcut COMMAND + SHIFT + K while simulator has focus on text input")
+        }
+        _ = app.textFields[txtArtistIdentify].waitForExistence(timeout: 10)
+        app.textFields[txtArtistIdentify].typeText(artist)
+        
+        app.textFields[txtSongTitleIdentify].tap()
+        if !app.keys["a"].waitForExistence(timeout: 5) {
+            XCTFail("The keyboard could not be found. Use keyboard shortcut COMMAND + SHIFT + K while simulator has focus on text input")
+        }
+        _ = app.textFields[txtSongTitleIdentify].waitForExistence(timeout: 10)
+        app.textFields[txtSongTitleIdentify].typeText(songTitle)
+        
+        app.buttons[btnSearchLyricIdentify].tap()
+        
+        XCTAssertTrue(app.otherElements[viewLyricDetailIdentify].waitForExistence(timeout: 5))
+    }
+}
